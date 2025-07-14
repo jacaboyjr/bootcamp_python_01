ad1009bd1053b4be7d1509db8fa945d415ad107e
wjunior <junior.wander@gmail.com>
2025-07-13 22:09:21 -0300

    Refatorando o main

--- main.py
+++ main.py
@@ -13,12 +13,13 @@ valor_02 = float(input("Digite o segundo valor: "))
 soma = valor_01 + valor_02
 print(f"A soma dos dois valores é: {soma}")
 '''
+
+CONTANTE_BONUS = 1000
+
 #desafio - Calculo de Bonus com Entrada do Usuário
 # Calculo de KPI do bônus de 2025 é de 1000 + salario + bonus
 nome_funcionario = input("Digite o nome do funcionário: ")
 salario = float(input("Digite o salário do funcionário: "))
 porcentagem_bonus = float(input("Digite a porcentagem do bônus: "))
-valor_bonus = salario * (porcentagem_bonus / 100)
-kpi_bonus = 1000 + salario + valor_bonus
-print(f"O valor do bônus é {valor_bonus:.2f}")
-print(f"O KPI do bônus de 2025 para {nome_funcionario} é: {kpi_bonus:.2f}")
+valor_bonus = CONTANTE_BONUS + salario * porcentagem_bonus
+print(f"O valor do bônus para {nome_funcionario} é: {valor_bonus:.2f}")
